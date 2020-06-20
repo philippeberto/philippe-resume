0c0d3bf4546aa8519d46b3df0c5b0cca46cdd7e4
Philippe Berto <philippeberto@ucl.br>
2020-06-20 10:41:52 +0100

    responsividade ajustada

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,35 @@
 import React from 'react'
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithubSquare } from 'react-icons/fa';
+import PageHead from '../components/head'
+import Hero from '../Hero/index'
+
 
 
 const Index = (props) => {
     return( 
-        <div className="container mx-auto w-4/6">
-            <div className='grid grid-cols-2'> 
-                <div className='mt-16 mt-16'>
-                    <h1 className="text-3xl uppercase leading-none">Olá, eu sou o Philippe Berto</h1>
-                    <h3 className="font-bold text-3xl uppercase leading-none">Fullstack Developer</h3>
-                    <div className='absolute border-2 border-green-500 rounded-lg p-2 mt-6 px-4'>
-                        <h3 className='absolute rounded-lg text-xl text-white uppercase font-bold bg-green-500 px-2 -mt-6 ml-2'>Contato</h3>
-                        <p className='mt-4 ml-2'>
-                            <FaLinkedin className='text-4xl inline-block' />
-                            <FaGithubSquare className='text-4xl inline-block ml-4' /><br />
-                            <span className='inline-block mt-2 text-sm'>Ou deixe um email: philippeberto@outlook.com</span>
-                            
-                        </p>
-                    </div>
-                </div>
-                <div className='w-1/2 flex items-start'>
-                    <img className='float-right items-end mt-32' src='images/back.png' alt='minha imagem' /> 
-                    <img className='mt-8' src='images/logos_transparent.png' alt='logos' />                   
-                </div> 
-            </div>
+        <div className="container mx-auto">
+            <PageHead />
+            <Hero />
             
             <div className=' bg-white p-4 text-center rounded-lg shadow-lg'>
                 <p>Programador Fullstack, desenvolvo sites e sistemas web baseados nas tecnologias Javascript, Node.js e React.</p>
             </div>
 
-            <h3 className='text-center py-4 text-3xl uppercase text-green-500 font-bold'>Minha Formação</h3>
+            <h3 className='text-center py-4 text-xl md:text-3xl uppercase text-green-500 font-bold'>Minha Formação</h3>
             <div className=' bg-white p-4 rounded-lg shadow-lg'>
                 <div className='grid grid-cols-2 ml-6'>
                     <div>
                         <h2 className='text-green-500 uppercase'>Graduação</h2>
-                        <h3 className='text-xl uppercase'>Análise e Desenvolvimento de Sistemas</h3>
-                        <h4 className='text-sm'>UCL - Faculdade do Centro Leste, Serra - Brasil</h4>
+                        <h3 className='text-sm md:text-xl uppercase'>Análise e Desenvolvimento de Sistemas</h3>
+                        <h4 className='text-xs md:text-sm'>UCL - Faculdade do Centro Leste, Serra - Brasil</h4>
                     </div>
                     <div className='ml-10'>
-                        <h2 className='text-green-500 uppercase'>Extras</h2>
-                        <h3 className='text-xl uppercase'>Fulstack Master</h3>
-                        <h4 className='text-sm'>Curso Online - DevPleno</h4>  
+                        <h2 className='text-sm md:text-xl text-green-500 uppercase'>Extras</h2>
+                        <h3 className='text-sm md:text-xl uppercase'>Fulstack Master</h3>
+                        <h4 className='text-xs md:text-sm'>Curso Online - DevPleno</h4>  
                     </div>           
                 </div>
             </div>
-            <h3 className='text-center mt-4 text-3xl uppercase text-green-500 font-bold'>Minhas Contribuições</h3>
+            <h3 className='text-center mt-4 text-xl md:text-3xl uppercase text-green-500 font-bold'>Minhas Contribuições</h3>
             <p className='text-center'>Public Repos: {props.user.public_repos} | Public Gists: {props.user.public_gists} | Followers: {props.user.followers}</p>
                     
             {props.repos.map (repo => {
@@ -60,7 +44,7 @@ const Index = (props) => {
             <div className='text-center text-sm mt-2'>
                 <p>Este site foi criado durante o curso Fullstack Master. Foram utilizados NextJS + SSR (Server Side Rendering)<br />
                 + Vercel (como platforma). Tu podes encontrar o código-fonte deste projeto em:: <br />
-                https://github.com/philippeberto/philippe-resume</p>            
+                <a href='https://github.com/philippeberto/philippe-resume'>https://github.com/philippeberto/philippe-resume</a></p>            
             </div>
         </div>
     )
