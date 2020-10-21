88725f50060be071c2aee330608d1ca1dab5dafb
Philippe <philippe.bjj@gmail.com>
2020-10-21 13:44:30 +0100

    ajuste do sumário

--- components/Summary.js
+++ components/Summary.js
@@ -6,7 +6,7 @@ const Summary = () => {
             <p>Sou capaz de desenvolver sites e contribuir no desenvolvimento de 
                 sistemas web, tanto front-end como back-end, através de Javascript 
                 e Node.js. Minhas principais ferramentas neste contexto são Next.js, 
-                Express, Gatsby, Auth0, Jwt, MySQL, MongoDB e Cloud Firestore, dentre outras.</p>
+                Express, Auth0, Jwt, MySQL, MongoDB e Cloud Firestore, dentre outras.</p>
         </div>
     )
 }
